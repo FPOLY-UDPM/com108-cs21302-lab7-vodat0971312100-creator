e97a401bf9e5c887ba324a89f85d358621cdfa1a
vodat0971312100 <vodat0971312100@gmail.com>
2026-02-28 13:00:31 +0700

    lab7

--- bai1.c
+++ bai1.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Nguyễn Võ Minh Đạt]
+ * MSSV:      [PS49299]
+ * Lớp:       [COM108]
  *****************************************************************************/
 
 //  BÀI 1: XÂY DỰNG CHƯƠNG TRÌNH ĐẾM NGUYÊN ÂM VÀ PHỤ ÂM CỦA 1 CHUỖI 
@@ -11,4 +11,36 @@
 
 
 // VIẾT CODE Ở ĐÂY
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char tiepTuc;
+    char chuoi[100];
+    int phuAm = 0, nguyenAm = 0;
 
+    printf("Bạn hãy nhập vào chuỗi ký tự cần kiểm tra: \n");
+    fgets(chuoi, sizeof(chuoi), stdin);
+    for(int i = 0; i < strlen(chuoi); i++)
+    {
+        if(chuoi[i] == 'a' || chuoi[i] == 'e' || chuoi[i] == 'u' || chuoi[i] == 'i' || chuoi[i] == 'o' || chuoi[i] == 'y'
+        || chuoi[i] == 'A' || chuoi[i] == 'E' || chuoi[i] == 'U' || chuoi[i] == 'I' || chuoi[i] == 'O' || chuoi[i] == 'Y' )
+        {
+            nguyenAm = nguyenAm + 1;
+        }
+        else if (chuoi[i] == ' ')
+        {
+            continue;
+        }
+        else phuAm = phuAm + 1;
+    }
+
+    printf("\nChuỗi bạn vừa nhập có %d ký tự nguyên âm\n", nguyenAm);
+    printf("Chuỗi bạn vừa nhập có %d ký tự phụ âm\n", phuAm);
+
+    printf("\nBạn có muốn tiếp tục không? (y/n): ");
+    scanf("%c", &tiepTuc);
+    getchar();
+    if(tiepTuc == 'y' || tiepTuc == 'Y');
+    return 0;
+}
